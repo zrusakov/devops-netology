6e41a93cf5875c56fa7f13b7534432260ccb1281
zrusakov <zrusakov@gmail.com>
2023-10-01 11:44:07 +0000

    clean branch

--- ansible.tf
+++
@@ -1,9 +0,0 @@
-resource "local_file" "ansible_inventory" {
-  filename = "./ansible_inventory"
-  content  = templatefile("${path.module}/inventory.tftpl", {
-    webservers = yandex_compute_instance.web,
-    databases = yandex_compute_instance.db,
-    storage = yandex_compute_instance.storage
-})
-}
-
--- local.tf
+++
@@ -1,5 +0,0 @@
-locals {
-spe = "1"
-key = "ubuntu:ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINiFCb8W4biGF2NZpeR3gAy18yOBfpL0uozhmngmWDdu vagrant@server1"
-}
-
